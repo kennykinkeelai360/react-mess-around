89a2daf6ffe51280002a39ade6bbd41891e582af
Kenny Lai <kenny.lai@ctt.co.uk>
2022-04-28 15:07:45 +0100

    Updated so the menu displays and hides

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTransition, animated } from "react-spring";
 
 function Navigation() {
+
+  //Internal state of the component with variable of showMenu and it's SETTER name of setShowMenu
+  const [showMenu, setShowMenu] = useState(false);
+
+  // a function of it's own made use in the nav icon
+  function toggleShowMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  function disableMenu() {
+    setShowMenu(false);
+  }
+
+  //className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+  //mask className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+  let menu;
+  let menuMask;
+
+  if (showMenu === true) {
+    menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+      The Menu
+    </div>
+
+    menuMask = <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+    onClick={disableMenu}>
+
+    </div>
+  }
+
     return (
         <nav>
             <FontAwesomeIcon
                 icon={faBars}
+                onClick={toggleShowMenu}
             />
+          {menuMask}
+          {menu}
+
         </nav>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // you can add custom css here with custom class names
+      backgroundColor: {
+        'black-t-50': 'rgba(0,0,0,0.5)'
+      }
+    },
   },
   plugins: [],
 }
